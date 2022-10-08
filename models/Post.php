8137e10d55dad44a0bdbf24a801dac5c4076c161
Peturik <peturik@gmail.com>
2022-10-08 10:44:26 +0200

    Added model Post

---
+++ models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $article
+ * @property string $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'article', 'author'], 'required'],
+            [['article'], 'string'],
+            [['title', 'author'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'article' => 'Article',
+            'author' => 'Author',
+        ];
+    }
+}
